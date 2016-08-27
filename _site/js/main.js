8009c68c5cb109603811e70851c88df1adbdf30f
Ulises Santana <ulisesantana@gmail.com>
2016-08-27 21:23:35 +0100

    Changing typography

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,6 +1,3 @@
 $(document).ready(function() {
-  $('.menu-burger, .menu-items').on('click', function() {
-  $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
-  $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
-});
+  
 });
