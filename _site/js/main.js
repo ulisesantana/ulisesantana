29d0a08373a998a7d17a88807dcdf22b268df2c2
Ulises Santana <ulisesantana@gmail.com>
2016-08-27 20:55:36 +0100

    Trying new menu

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,2 +1,6 @@
 $(document).ready(function() {
+  $('.menu-burger, .menu-items').on('click', function() {
+  $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
+  $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
+});
 });
