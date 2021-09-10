9a0bb1ae6c8dbeb4ef2d1af81f6e1b5f3c995898
Ulises Santana <ulisesantana@gmail.com>
2021-09-10 21:11:29 +0100

    💄 increase max image width inside a post

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
                     {
                         resolve: `gatsby-remark-images`,
                         options: {
-                            maxWidth: 590,
+                            maxWidth: 960,
                             linkImagesToOriginal: false,
                         },
                     },
