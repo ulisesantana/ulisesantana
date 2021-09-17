84b038f4ce49128f0d47e8877f9a536a1bece6fe
Ulises Santana <ulisesantana@gmail.com>
2021-09-17 05:27:08 +0100

    💄 fix titles for cv

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -18,18 +18,18 @@ const SectionTitleWrapper = styled.div`
   margin: 2rem 0 1rem;
   padding: 1rem;
 
-  h2, span {
+  h2.title, span.title {
     color: var(--black);
     display: inline-flex;
     font-size: 2rem;
     line-height: 2rem;
   }
   
-  span {
+  span.title {
     padding-right: 1rem;
   }
 
-  h2 {
+  h2.title {
     margin: 0;
   }
 `
@@ -37,34 +37,36 @@ const SectionWrapper = styled.section`
   font-size: 1rem;
   line-height: 1rem;
 
-  p {
-    font-size: 1rem;
-    line-height: 1.5rem;
-    margin: 1rem 0;
-    max-width: 100%;
-  }
-
   .content {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
+
+    p {
+      font-size: 1rem;
+      line-height: 1.5rem;
+      margin: 1rem 0;
+      max-width: 100%;
+    }
   }
 
   @media print, screen and (max-width: 420px) {
     font-size: 14px;
     line-height: 20px;
 
-    p {
-      font-size: 14px;
-      line-height: 20px;
+    .content {
+      p {
+        font-size: 14px;
+        line-height: 20px;
+      }  
     }
   }
 `
 
 const SectionTitle: FC<SectionProps> = ({ title, icon }) => (
   <SectionTitleWrapper>
-    <span>{icon}</span>
-    <h2>{title}</h2>
+    <span className="title">{icon}</span>
+    <h2 className="title">{title}</h2>
   </SectionTitleWrapper>
 )
 
